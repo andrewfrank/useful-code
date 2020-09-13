5168ad269ab1c58cae81c594e7e69dd2561dd0a4
Andrew Frank <Andrew@louise.local>
2020-09-13 05:42:35 -0400

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# install tools for bioinformatics development on macOS High Sierra workstation
+
+# this shell script is less meant to be run on a new workstation, but more as
+# a running list of packages and tools essential for bioinformatics development
+# (with a strong bent toward microbiology) with references
+
+# install homebrew
+# https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# install sane build environment for macOS
+brew install openssl
+brew install readline
+brew install sqlite3
+brew install xz
+brew install zlib
+
+# install developer packages
+brew install git
+brew install shellcheck
+brew install pigz
+brew install pyenv
+brew install pyenv-virtualenv
+brew install pylint
+brew install r
+
+# install IDEs
+brew cask install visual-studio-code
+brew cask install rstudio
+
+# install bioinformatics packages
+brew install brewsci/bio/FORMULA
+brew install fastqc
+brew install spades
+
+# configure python using pyenv + pyenv-virtualenv
+# https://stackoverflow.com/questions/41573587 for virtues of pyenv/virtualenv
+# https://github.com/pyenv/pyenv#homebrew-on-macos
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+source ~/.bash_profile
+version="3.7.3"
+pyenv install $version
+pyenv global $version
+# https://github.com/pyenv/pyenv-virtualenv#installing-with-homebrew-for-macos-users
+echo -e 'if which pyenv-virtualenv-init > /dev/null; then\n  eval "$(pyenv virtualenv-init -)"\nfi' >> ~/.bash_profile
+source ~/.bash_profile
+# bootstrap existing pip to the latest version of pip
+python -m pip install --user --upgrade pip
+
+# requires running the following in the command palette for VS Code command line
+# `>Shell Command: Install 'code' command in PATH` in VS Code
+
+# set up vscode packages
+code --install-extension timonwong.shellcheck
+code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
